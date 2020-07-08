9d3b20b10a258645d60601b37fff5268436f10a7
jminieri <jminieri@mitre.org>
2020-07-08 08:42:04 -0400

    Parse PDF

--- static/bundle.js
+++ static/bundle.js
@@ -63,8 +63,13 @@ $(function() {
                 }
                 else if (data["entry"][i]["resource"]["resourceType"] == "CarePlan") {
                   let careplan = data["entry"][i]["resource"];
-                  div += "<h3>Care Plan</h3>"+data["entry"][i]["resource"]["text"]["div"];
+                  div += "<h3>Care Plan</h3>";
 
+                  if (data["entry"][i]["resource"]["text"]) {
+                      div += "<h3>Care Plan</h3>"+data["entry"][i]["resource"]["text"]["div"];
+                  } else {
+                      console.log(data["entry"][i])
+                  }
                   for (let j = 0; j < careplan["activity"].length; j++) {
                       if (careplan["activity"][j]["detail"]["kind"] === "MedicationRequest") {
                           medDisplay += "<h3>"+careplan["activity"][j]["detail"]["kind"]+"</h3><table style='width:100%'>";
@@ -121,6 +126,7 @@ $(function() {
                     } else if (docRef["content"][0]["attachment"]["contentType"] === "application/pdf") {
                         // Parse the base64 PDF
                         supportingInfo += "<tr><td>PDF Attachment</td></tr>";
+                        convertToPDF(docRef["content"][0]["attachment"]["data"]);
                     }
                     supportingInfo += "</table>";
                 }
@@ -129,7 +135,6 @@ $(function() {
               $("#medicineContent").html(medDisplay);
               $("#equipmentContent").html(deviceDisplay);
               $("#other").html(other);
-              console.log(supportingInfo)
               $("#supInf").html(supportingInfo);
               $("#bundle").html(div);
           }
@@ -137,6 +142,33 @@ $(function() {
     return false;
   });
 });
+function convertToPDF(b64) {
+      // The Base64 string of a simple PDF file
+      // Decode Base64 to binary and show some information about the PDF file (note that I skipped all checks)
+      let bin = atob(b64);
+      console.log('File Size:', Math.round(bin.length / 1024), 'KB');
+      console.log('PDF Version:', bin.match(/^.PDF-([0-9.]+)/)[1]);
+      console.log('Create Date:', bin.match(/<xmp:CreateDate>(.+?)<\/xmp:CreateDate>/)[1]);
+      console.log('Modify Date:', bin.match(/<xmp:ModifyDate>(.+?)<\/xmp:ModifyDate>/)[1]);
+      console.log('Creator Tool:', bin.match(/<xmp:CreatorTool>(.+?)<\/xmp:CreatorTool>/)[1]);
+
+      // Embed the PDF into the HTML page and show it to the user
+      let obj = document.createElement('object');
+      obj.style.width = '100%';
+      obj.style.height = '842pt';
+      obj.type = 'application/pdf';
+      obj.data = 'data:application/pdf;base64,' + b64;
+      // document.body.appendChild(obj);
+      $("#pdfDiv").append(obj);
+
+      // Insert a link that allows the user to download the PDF file
+      let link = document.createElement('a');
+      link.innerHTML = 'Download PDF file';
+      link.download = 'file.pdf';
+      link.href = 'data:application/octet-stream;base64,' + b64;
+      $("#pdfDiv").append(link);
+      // document.body.appendChild(link);
+}
 function formatResource(data) {
     let formatter = null;
     if (data["resourceType"] == "OperationOutcome") {
